f26f2a3afa6ac5a577eafebb9b29e0883381dbaa
lc8882972 <guoyan2972@qq.com>
2016-07-17 15:39:26 +0800

    remove nuget.exe

---
+++ src/Samples/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Samples.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : ApiBaseController
+    {
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            ClaimsIdentity identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim(ClaimTypes.Name, "guoyan"));
+            ClaimsPrincipal principal = new ClaimsPrincipal();
+            principal.AddIdentity(identity);
+            Context.Authentication.SignInAsync("SPA", principal);
+            return Ok();
+        }
+
+    }
+}
